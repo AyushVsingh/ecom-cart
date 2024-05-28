dd5c9e13c3f1287198307d80e246aafa7cd9a004
unknown <aayushsingh050801@gmail.com>
2024-05-28 09:48:39 +0530

    Added Navbar and Changed other components except App.js to functional component

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,45 +1,46 @@
 import React from 'react';
 
-class CartItem extends React.Component {
-    render() {
-        console.log('this.props', this.props);
-        const { price, title, qty, img, id } = this.props.product;
-        const { product, onIncreaseQuantity, onDecreaseQuantity, onDeleteQuantity } = this.props;
-        return (
-            <div className="cart-item">
-                {this.props.jsx}
-                <div className="left-block">
-                    <img style={styles.image} />
-                </div>
-                <div className="right-block">
-                    <div style={{ fontSize: 25 }}>{title}</div>
-                    <div style={{ color: '#777' }}>Rs {price} </div>
-                    <div style={{ color: '#777' }}>Qty: {qty} </div>
-                    <div className="cart-item-actions">
-                        {/* Buttons */}
-                        <img
-                            alt="increase"
-                            className="action-icons"
-                            src="https://www.svgrepo.com/show/507821/plus-circle.svg"
-                            onClick={() => onIncreaseQuantity(product)}
-                        />
-                        <img
-                            alt="decrease"
-                            className="action-icons"
-                            src="https://www.svgrepo.com/show/507785/minus-circle.svg"
-                            onClick={() => onDecreaseQuantity(product)}
-                        />
-                        <img
-                            alt="delete"
-                            className="action-icons"
-                            src="https://www.svgrepo.com/show/502608/delete-2.svg"
-                            onClick={() => onDeleteQuantity(product.id)}
-                        />
-                    </div>
+const CartItem = (props) => {
+    const { price, title, qty } = props.product;
+    const {
+        product,
+        onIncreaseQuantity,
+        onDecreaseQuantity,
+        onDeleteProduct
+    } = props;
+    return (
+        <div className="cart-item">
+            <div className="left-block">
+                <img style={styles.image} src={product.img} />
+            </div>
+            <div className="right-block">
+                <div style={{ fontSize: 25 }}>{title}</div>
+                <div style={{ color: '#777' }}>Rs {price} </div>
+                <div style={{ color: '#777' }}>Qty: {qty} </div>
+                <div className="cart-item-actions">
+                    {/* Buttons */}
+                    <img
+                        alt="increase"
+                        className="action-icons"
+                        src="https://www.svgrepo.com/show/507821/plus-circle.svg"
+                        onClick={() => onIncreaseQuantity(product)}
+                    />
+                    <img
+                        alt="decrease"
+                        className="action-icons"
+                        src="https://www.svgrepo.com/show/507785/minus-circle.svg"
+                        onClick={() => onDecreaseQuantity(product)}
+                    />
+                    <img
+                        alt="delete"
+                        className="action-icons"
+                        src="https://www.svgrepo.com/show/502608/delete-2.svg"
+                        onClick={() => onDeleteProduct(product.id)}
+                    />
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 const styles = {
